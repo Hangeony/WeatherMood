26f145771c2b3695216dbe60db3d5543fd3ad355
Hangeony <geonhui95@naver.com>
2025-07-27 20:07:22 +0900

    fix: swagger 문서수정

--- src/auth/dto/signup.response.ts
+++ src/auth/dto/signup.response.ts
@@ -1,9 +1,6 @@
 import { ApiProperty } from '@nestjs/swagger';
 
 export class SignupResponseDto {
-  @ApiProperty({ example: 1 })
-  id: number;
-
   @ApiProperty({ example: 'test@example.com' })
   email: string;
 
